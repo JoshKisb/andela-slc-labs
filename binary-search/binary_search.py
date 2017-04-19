fbeed950f56e58ddcad29b6b3c4c107ffcef41a6
Josh Kisb <joshuakisb@gmail.com>
2017-04-19 12:33:44 +0300

    change search algorithm to calculate midpoint using search value

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -4,7 +4,40 @@ class BinarySearch(list):
 		self.length = len(self)
 
 	def search(self, value):
-		pass
+		first = 0
+		last = len(self)-1
+		count = 0
+		midpoint = (value // 2) 
+		if midpoint > len(self):
+			midpoint = value // len(self)
+		# 	midpoint = (first + last) // 2
+		# while midpoint > len(self):
+		# 	midpoint = (midpoint // 2)
+		midpoint -= 1
+		while first <= last:
+			if self[midpoint] == value:
+				return {'count': count, 'index': midpoint}
+			else:
+				count += 1
+				if value < self[midpoint]:
+					last = midpoint-1
+				else:
+					first = midpoint+1
+			midpoint = (first + last)//2
+		return {'count': count, 'index': -1}
 
 if __name__ == '__main__':
-	print(BinarySearch(20, 2))
+	small_list = BinarySearch(20, 1)
+	print(small_list)
+	print("----- search 16 ------")
+	print(small_list.search(16))
+
+	medium_list = BinarySearch(20, 2)
+	print(medium_list)
+	print("----- search 40 ------")
+	print(medium_list.search(40))
+
+	large_list = BinarySearch(100, 10)
+	print(large_list)
+	print("----- search 880 ------")
+	print(large_list.search(880))

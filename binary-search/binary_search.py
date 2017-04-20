dd51d2c725fd2fb8918f605cd44012512b2ac93f
Josh Kisb <joshuakisb@gmail.com>
2017-04-20 09:51:41 +0300

    change len(self) to self.length

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -3,18 +3,15 @@ class BinarySearch(list):
 		list.__init__(self, [i for i in range(step, (length*step) + 1, step)])
 		self.length = len(self)
 
-	def search(self, value):
+	def search(self, value, midpo=0):
 		first = 0
-		last = len(self)-1
+		last = self.length - 1
 		count = 0
-		midpoint = (value // 2) 
-		if midpoint > len(self):
-			midpoint = value // len(self)
-		# 	midpoint = (first + last) // 2
-		# while midpoint > len(self):
-		# 	midpoint = (midpoint // 2)
-		midpoint -= 1
+
+		# midpoint = (value // 2) - 1
+
 		while first <= last:
+			midpoint = (first + last)//2
 			if self[midpoint] == value:
 				return {'count': count, 'index': midpoint}
 			else:
@@ -23,21 +20,5 @@ class BinarySearch(list):
 					last = midpoint-1
 				else:
 					first = midpoint+1
-			midpoint = (first + last)//2
+
 		return {'count': count, 'index': -1}
-
-if __name__ == '__main__':
-	small_list = BinarySearch(20, 1)
-	print(small_list)
-	print("----- search 16 ------")
-	print(small_list.search(16))
-
-	medium_list = BinarySearch(20, 2)
-	print(medium_list)
-	print("----- search 40 ------")
-	print(medium_list.search(40))
-
-	large_list = BinarySearch(100, 10)
-	print(large_list)
-	print("----- search 880 ------")
-	print(large_list.search(880))
